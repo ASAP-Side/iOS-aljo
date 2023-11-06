d55ed78cd2d2b1681645278e490831f9fd47ae46
미니 <cow970814@naver.com>
2023-11-06 18:27:43 +0900

    feat : start Project

---
+++ aljo/Targets/AljoUI/Sources/AljoUI.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public final class AljoUI {
+    public static func hello() {
+        print("Hello, from your UI framework")
+    }
+}
---
+++ aljo/Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -0,0 +1,141 @@
+import ProjectDescription
+
+/// Project helpers are functions that simplify the way you define your project.
+/// Share code to create targets, settings, dependencies,
+/// Create your own conventions, e.g: a func that makes sure all shared targets are "static frameworks"
+/// See https://docs.tuist.io/guides/helpers/
+///
+
+extension Project {
+    static let appName = "Aljo"
+    static let organizationName = "ASAP.com"
+    
+    static func app(
+        name: String,
+        platform: Platform,
+        targets: [String]
+    ) -> Project {
+        let dependencies = targets.map { TargetDependency.target(name: $0) }
+        var targets = makeAppTargets(
+            name: name,
+            platform: platform,
+            dependencies: dependencies
+        )
+        
+        return Project(
+            name: appName,
+            organizationName: organizationName,
+            targets: targets
+        )
+    }
+}
+//
+//extension Project {
+//    /// Helper function to create the Project for this ExampleApp
+//    public static func app(
+//        name: String,
+//        platform: Platform,
+//        additionalTargets: [String]
+//    ) -> Project {
+//        var targets = makeAppTargets(name: name,
+//                                     platform: platform,
+//                                     dependencies: additionalTargets.map {
+//            TargetDependency.target(
+//                name: $0
+//            )
+//        })
+//        targets += additionalTargets.flatMap({
+//            makeFrameworkTargets(
+//                name: $0,
+//                platform: platform
+//            )
+//        })
+//        return Project(
+//            name: name,
+//            organizationName: "tuist.io",
+//            targets: targets
+//        )
+//    }
+//    
+//    // MARK: - Private
+//    
+//    /// Helper function to create a framework target and an associated unit test target
+//    private static func makeFrameworkTargets(
+//        name: String,
+//        platform: Platform
+//    ) -> [Target] {
+//        let sources = Target(
+//            name: name,
+//            platform: platform,
+//            product: .framework,
+//            bundleId: "io.tuist.\(name)",
+//            infoPlist: .default,
+//            sources: ["Targets/\(name)/Sources/**"],
+//            resources: [],
+//            dependencies: []
+//        )
+//        
+//        let tests = Target(
+//            name: "\(name)Tests",
+//            platform: platform,
+//            product: .unitTests,
+//            bundleId: "io.tuist.\(name)Tests",
+//            infoPlist: .default,
+//            sources: ["Targets/\(name)/Tests/**"],
+//            resources: [],
+//            dependencies: [.target(
+//                name: name
+//            )]
+//        )
+//        return [
+//            sources,
+//            tests
+//        ]
+//    }
+//    
+//    /// Helper function to create the application target and the unit test target.
+//    private static func makeAppTargets(
+//        name: String,
+//        platform: Platform,
+//        dependencies: [TargetDependency]
+//    ) -> [Target] {
+//        let platform: Platform = platform
+//        let infoPlist: [String: Plist.Value] = [
+//            "CFBundleShortVersionString": "1.0",
+//            "CFBundleVersion": "1",
+//            "UIMainStoryboardFile": "",
+//            "UILaunchStoryboardName": "LaunchScreen"
+//        ]
+//        
+//        let mainTarget = Target(
+//            name: name,
+//            platform: platform,
+//            product: .app,
+//            bundleId: "io.tuist.\(name)",
+//            infoPlist: .extendingDefault(
+//                with: infoPlist
+//            ),
+//            sources: ["Targets/\(name)/Sources/**"],
+//            resources: ["Targets/\(name)/Resources/**"],
+//            dependencies: dependencies
+//        )
+//        
+//        let testTarget = Target(
+//            name: "\(name)Tests",
+//            platform: platform,
+//            product: .unitTests,
+//            bundleId: "io.tuist.\(name)Tests",
+//            infoPlist: .default,
+//            sources: ["Targets/\(name)/Tests/**"],
+//            dependencies: [
+//                .target(
+//                    name: "\(name)"
+//                )
+//            ]
+//        )
+//        return [
+//            mainTarget,
+//            testTarget
+//        ]
+//    }
+//}
