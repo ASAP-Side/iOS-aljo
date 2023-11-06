2d05f932f8337d5493e432984ded2a3d0412f4fb
미니 <cow970814@naver.com>
2023-11-06 18:32:45 +0900

    feat : Project 설정 추가

--- aljo/Project.swift
+++ aljo/Project.swift
@@ -2,28 +2,8 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 import MyPlugin
 
-/*
-                +-------------+
-                |             |
-                |     App     | Contains Aljo App target and Aljo unit-test target
-                |             |
-         +------+-------------+-------+
-         |         depends on         |
-         |                            |
- +----v-----+                   +-----v-----+
- |          |                   |           |
- |   Kit    |                   |     UI    |   Two independent frameworks to share code and start modularising your app
- |          |                   |           |
- +----------+                   +-----------+
-
- */
-
-// MARK: - Project
-
 // Local plugin loaded
 let localHelper = LocalHelper(name: "MyPlugin")
 
 // Creates our project using a helper function defined in ProjectDescriptionHelpers
-let project = Project.app(name: "Aljo",
-                          platform: .iOS,
-                          additionalTargets: ["AljoKit", "AljoUI"])
+let project = Project.app(name: "Aljo", platform: .iOS, targets: [])
