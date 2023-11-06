17e47226e6edbbd33a1cfd52c26c2782b445e959
미니 <cow970814@naver.com>
2023-11-06 23:31:48 +0900

    refactor : Tuist 버전 변경

--- aljo/Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ aljo/Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -4,39 +4,46 @@ import ProjectDescription
 /// Share code to create targets, settings, dependencies,
 /// Create your own conventions, e.g: a func that makes sure all shared targets are "static frameworks"
 /// See https://docs.tuist.io/guides/helpers/
-///
 
 extension Project {
-    static let appName = "Aljo"
-    static let organizationName = "ASAP.com"
+    /// Helper function to create the Project for this ExampleApp
+    public static func app(name: String, platform: Platform, additionalTargets: [String]) -> Project {
+        var targets = makeAppTargets(name: name,
+                                     platform: platform,
+                                     dependencies: additionalTargets.map { TargetDependency.target(name: $0) })
+        targets += additionalTargets.flatMap({ makeFrameworkTargets(name: $0, platform: platform) })
+        return Project(name: name,
+                       organizationName: "tuist.io",
+                       targets: targets)
+    }
     
-    public static func app(
-        name: String,
-        platform: Platform,
-        targets: [String]
-    ) -> Project {
-        let dependencies = targets.map { TargetDependency.target(name: $0) }
-        let targets = makeAppTargets(
-            name: name,
-            platform: platform,
-            dependencies: dependencies
-        )
-        
-        return Project(
-            name: appName,
-            organizationName: organizationName,
-            targets: targets
-        )
+    // MARK: - Private
+    
+    /// Helper function to create a framework target and an associated unit test target
+    private static func makeFrameworkTargets(name: String, platform: Platform) -> [Target] {
+        let sources = Target(name: name,
+                             platform: platform,
+                             product: .framework,
+                             bundleId: "io.tuist.\(name)",
+                             infoPlist: .default,
+                             sources: ["Targets/\(name)/Sources/**"],
+                             resources: [],
+                             dependencies: [])
+        let tests = Target(name: "\(name)Tests",
+                           platform: platform,
+                           product: .unitTests,
+                           bundleId: "io.tuist.\(name)Tests",
+                           infoPlist: .default,
+                           sources: ["Targets/\(name)/Tests/**"],
+                           resources: [],
+                           dependencies: [.target(name: name)])
+        return [sources, tests]
     }
     
     /// Helper function to create the application target and the unit test target.
-    private static func makeAppTargets(
-        name: String,
-        platform: Platform,
-        dependencies: [TargetDependency]
-    ) -> [Target] {
+    private static func makeAppTargets(name: String, platform: Platform, dependencies: [TargetDependency]) -> [Target] {
         let platform: Platform = platform
-        let infoPlist: [String: Plist.Value] = [
+        let infoPlist: [String: InfoPlist.Value] = [
             "CFBundleShortVersionString": "1.0",
             "CFBundleVersion": "1",
             "UIMainStoryboardFile": "",
@@ -48,9 +55,7 @@ extension Project {
             platform: platform,
             product: .app,
             bundleId: "io.tuist.\(name)",
-            infoPlist: .extendingDefault(
-                with: infoPlist
-            ),
+            infoPlist: .extendingDefault(with: infoPlist),
             sources: ["Targets/\(name)/Sources/**"],
             resources: ["Targets/\(name)/Resources/**"],
             dependencies: dependencies
@@ -64,80 +69,8 @@ extension Project {
             infoPlist: .default,
             sources: ["Targets/\(name)/Tests/**"],
             dependencies: [
-                .target(
-                    name: "\(name)"
-                )
-            ]
-        )
-        return [
-            mainTarget,
-            testTarget
-        ]
+                .target(name: "\(name)")
+            ])
+        return [mainTarget, testTarget]
     }
 }
-//
-//extension Project {
-//    /// Helper function to create the Project for this ExampleApp
-//    public static func app(
-//        name: String,
-//        platform: Platform,
-//        additionalTargets: [String]
-//    ) -> Project {
-//        var targets = makeAppTargets(name: name,
-//                                     platform: platform,
-//                                     dependencies: additionalTargets.map {
-//            TargetDependency.target(
-//                name: $0
-//            )
-//        })
-//        targets += additionalTargets.flatMap({
-//            makeFrameworkTargets(
-//                name: $0,
-//                platform: platform
-//            )
-//        })
-//        return Project(
-//            name: name,
-//            organizationName: "tuist.io",
-//            targets: targets
-//        )
-//    }
-//
-//    // MARK: - Private
-//
-//    /// Helper function to create a framework target and an associated unit test target
-//    private static func makeFrameworkTargets(
-//        name: String,
-//        platform: Platform
-//    ) -> [Target] {
-//        let sources = Target(
-//            name: name,
-//            platform: platform,
-//            product: .framework,
-//            bundleId: "io.tuist.\(name)",
-//            infoPlist: .default,
-//            sources: ["Targets/\(name)/Sources/**"],
-//            resources: [],
-//            dependencies: []
-//        )
-//
-//        let tests = Target(
-//            name: "\(name)Tests",
-//            platform: platform,
-//            product: .unitTests,
-//            bundleId: "io.tuist.\(name)Tests",
-//            infoPlist: .default,
-//            sources: ["Targets/\(name)/Tests/**"],
-//            resources: [],
-//            dependencies: [.target(
-//                name: name
-//            )]
-//        )
-//        return [
-//            sources,
-//            tests
-//        ]
-//    }
-//
-
-//}
