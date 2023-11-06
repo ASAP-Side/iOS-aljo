f790fc58f02c3d3d396854b4bb13d81c54ec5c56
미니 <cow970814@naver.com>
2023-11-06 18:27:43 +0900

    feat : start Project

---
+++ aljo/Targets/AljoKit/Tests/AljoKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class AljoKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("AljoKit", "AljoKit")
+    }
+}
---
+++ aljo/Targets/AljoUI/Tests/AljoUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class AljoUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("AljoUI", "AljoUI")
+    }
+}
