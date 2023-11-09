5db1751c581a2315018304c43acc32847434c7af
미니 <cow970814@naver.com>
2023-11-10 00:15:28 +0900

    config : 3rd Party 추가

---
+++ aljo/Projects/Shared/3rdPartyLibrary/Project.swift
@@ -0,0 +1,27 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+import MyPlugin
+
+/*
+                +-------------+
+                |             |
+                |     App     | Contains 3rdPartyLibrary App target and 3rdPartyLibrary unit-test target
+                |             |
+         +------+-------------+-------+
+         |         depends on         |
+         |                            |
+ +----v-----+                   +-----v-----+
+ |          |                   |           |
+ |   Kit    |                   |     UI    |   Two independent frameworks to share code and start modularising your app
+ |          |                   |           |
+ +----------+                   +-----------+
+
+ */
+
+// MARK: - Project
+
+// Local plugin loaded
+let localHelper = LocalHelper(name: "MyPlugin")
+
+// Creates our project using a helper function defined in ProjectDescriptionHelpers
+let project = Project(name: "3rdPartyLibrary")
