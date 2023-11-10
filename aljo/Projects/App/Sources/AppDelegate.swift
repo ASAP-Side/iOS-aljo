1ede5b16357d4a9a903ae9b504710a4eb28a9010
미니 <cow970814@naver.com>
2023-11-10 23:38:53 +0900

    config : Swift Lint Script 변경

--- aljo/Projects/App/Sources/AppDelegate.swift
+++ aljo/Projects/App/Sources/AppDelegate.swift
@@ -13,7 +13,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
   func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
     window = UIWindow(frame: UIScreen.main.nativeBounds)
     let controller = HomeViewController()
