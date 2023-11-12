74f07cbfe686e9ce311888f06629f73244969113
미니 <cow970814@naver.com>
2023-11-12 14:58:43 +0900

    config : 프로젝트 설정 Extension 파일 구분

---
+++ aljo/Tuist/ProjectDescriptionHelpers/Project + Templates.swift
@@ -0,0 +1,26 @@
+import Foundation
+import ProjectDescription
+
+public extension Project {
+  static let organizationName: String = "ASAP"
+  static func app(to name: String, targets: @escaping () -> [Target]) -> Project {
+    return Project(
+      name: name,
+      organizationName: organizationName,
+      packages: [],
+      settings: nil,
+      targets: targets(),
+      schemes: [
+        Scheme(name: "DEBUG")
+      ],
+      fileHeaderTemplate: nil,
+      additionalFiles: [],
+      resourceSynthesizers: []
+    )
+  }
+}
+
+public extension SourceFilesList {
+  static let interface: SourceFilesList = "Interface/**"
+  static let sources: SourceFilesList = "Sources/**"
+}
