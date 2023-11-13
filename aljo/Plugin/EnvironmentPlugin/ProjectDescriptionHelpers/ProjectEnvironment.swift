2ef971a6a74e21526650961385cc2e11e2d2a164
미니 <cow970814@naver.com>
2023-11-14 01:45:01 +0900

    feat : Project 사이닝 추가

--- aljo/Plugin/EnvironmentPlugin/ProjectDescriptionHelpers/ProjectEnvironment.swift
+++ aljo/Plugin/EnvironmentPlugin/ProjectDescriptionHelpers/ProjectEnvironment.swift
@@ -10,7 +10,7 @@ public struct ProjectEnvironment {
 }
 
 public let environmentValues = ProjectEnvironment(
-  name: "Aljo",
+  name: "aljo",
   organizationName: "com.ASAP",
   deployTarget: .iOS(targetVersion: "14.0", devices: .iphone),
   platform: .iOS,
