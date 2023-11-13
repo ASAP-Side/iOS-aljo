67356a85afc6880dea817934b7b89bd0f545a01e
미니 <cow970814@naver.com>
2023-11-13 17:48:08 +0900

    feat : 프로젝트 설정 관련 플러그인 구성

--- aljo/Projects/App/Project.swift
+++ aljo/Projects/App/Project.swift
@@ -5,12 +5,8 @@ import AljoPlugin
 let project = Project.app(to: "Application") {
   [
     .target("Application", to: .app) {
-      [
-        .project(target: "AuthFeature", path: .relativeToRoot("Projects/Features/AuthFeature")),
-        .project(target: "HomeFeature", path: .relativeToRoot("Projects/Features/HomeFeature")),
-        .project(target: "DashBoardFeature", path: .relativeToRoot("Projects/Features/DashBoardFeature")),
-        .project(target: "MyPageFeature", path: .relativeToRoot("Projects/Features/MyPageFeature"))
-      ]
+      return ModulePaths.Feature.allCases.map { TargetDependency.feature(target: $0) }
+      + ModulePaths.Domain.allCases.map { TargetDependency.domain(target: $0) }
     }
   ]
 }
