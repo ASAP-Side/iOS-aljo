98cfca584bc998dfd068f80e0206cfb242b50260
미니 <cow970814@naver.com>
2023-11-27 15:25:58 +0900

    docs : 불필요한 주석 삭제

--- aljo/Projects/Features/HomeFeature/Project.swift
+++ aljo/Projects/Features/HomeFeature/Project.swift
@@ -1,7 +1,6 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-// Creates our project using a helper function defined in ProjectDescriptionHelpers
 let project = Project.app(to: "HomeFeature") {
   [
     .implements(module: .feature(.HomeFeature)) {
