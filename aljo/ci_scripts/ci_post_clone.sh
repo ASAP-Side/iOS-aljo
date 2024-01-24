ade0a7356fe04099ea103c44fb881ea84820276f
미니 <cow970814@naver.com>
2024-01-24 17:08:00 +0900

    feat : 스크립트 내 Tuist 명령어 추가

--- aljo/ci_scripts/ci_post_clone.sh
+++ aljo/ci_scripts/ci_post_clone.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 # PC에 swiftLint 설치하도록 하는 명령어
-brew install swiftlint
+# brew install swiftlint
+
+# Tuist 설치 명령어
+echo "Install Tuist"
+curl -Ls https://install.tuist.io | bash
 
 # 현재 디렉토리에서 프로젝트 디렉토리로 나가도록 설정
 cd ..
 
-# Tuist로 의존성 초기화 및 실행
+# Tuist로 의존성 설치
+echo "Update Tuist Dependencies"
 tuist fetch
+
+# Tuist 프로젝트 생성
+echo "Generate Tuist XCWorkSpace"
 tuist generate
