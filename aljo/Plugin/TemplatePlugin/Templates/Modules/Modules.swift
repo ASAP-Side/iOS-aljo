9845548730610b1594e798b52e47c9dd5544b132
lxodud <xodud1998223@naver.com>
2024-01-26 16:45:22 +0900

    feat: uFeature Template 구현

--- aljo/Plugin/TemplatePlugin/Templates/Modules/Modules.swift
+++ aljo/Plugin/TemplatePlugin/Templates/Modules/Modules.swift
@@ -1,7 +1,7 @@
 import ProjectDescription
 
-private let layerAttribute = Template.Attribute.required("l")
-private let nameAttribute = Template.Attribute.required("n")
+private let layerAttribute = Template.Attribute.required("layer")
+private let nameAttribute = Template.Attribute.required("name")
 
 private let template = Template(
     description: "A template for a new module",
--- aljo/Plugin/TemplatePlugin/Templates/uFeature/uFeature.swift
+++ aljo/Plugin/TemplatePlugin/Templates/uFeature/uFeature.swift
@@ -5,4 +5,49 @@
 //  Created by 이태영 on 1/26/24.
 //
 
-import Foundation
+import ProjectDescription
+
+private let layerAttribute = Template.Attribute.required("layer")
+private let nameAttribute = Template.Attribute.required("name")
+
+private let template = Template(
+    description: "A template for new uFeature",
+    attributes: [
+        layerAttribute,
+        nameAttribute,
+    ],
+    items: [
+        .file(
+            path: "Projects/\(layerAttribute)/\(nameAttribute)/Project.swift",
+            templatePath: "../Modules/Project.stencil"
+        ),
+        .file(
+            path: "Projects/\(layerAttribute)/\(nameAttribute)/Demo/SceneDelegate.swift",
+            templatePath: "../Demo/SceneDelegate.stencil"
+        ),
+        .file(
+            path: "Projects/\(layerAttribute)/\(nameAttribute)/Demo/AppDelegate.swift",
+            templatePath: "../Demo/AppDelegate.stencil"
+        ),
+        .file(
+            path: "Projects/\(layerAttribute)/\(nameAttribute)/Demo/LaunchScreen.storyboard",
+            templatePath: "../Demo/LaunchScreen.stencil"
+        ),
+        .file(
+            path: "Projects/\(layerAttribute)/\(nameAttribute)/Interface/Interface.swift",
+            templatePath: "../Interface/Interface.stencil"
+        ),
+        .file(
+            path: "Projects/\(layerAttribute)/\(nameAttribute)/Testing/Testing.swift",
+            templatePath: "../Testing/Testing.stencil"
+        ),
+        .file(
+            path: "Projects/\(layerAttribute)/\(nameAttribute)/Tests/\(nameAttribute)Tests.swift",
+            templatePath: "../Tests/Tests.stencil"
+        ),
+        .file(
+            path: "Projects/\(layerAttribute)/\(nameAttribute)/Implementation/Implementation.swift",
+            templatePath: "../Implementation/Implementation.stencil"
+        )
+    ]
+)
