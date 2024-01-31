6f7edc8b1a2427736ea3097b83612bdc6e89c9a7
lxodud <xodud1998223@naver.com>
2024-01-31 23:51:57 +0900

    feat: Provider 구현

---
+++ aljo/Projects/Core/AJNetwork/Interface/Provider.swift
@@ -0,0 +1,17 @@
+//
+//  Provider.swift
+//  AJNetworkInterface
+//
+//  Created by 이태영 on 1/31/24.
+//  Copyright © 2024 com.ASAP. All rights reserved.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol Provider {
+  static func string(_ router: Router) -> Observable<String>
+  static func decodable<T: Decodable>(_ router: Router) -> Observable<T>
+  static func data(_ router: Router) -> Observable<Data>
+}
