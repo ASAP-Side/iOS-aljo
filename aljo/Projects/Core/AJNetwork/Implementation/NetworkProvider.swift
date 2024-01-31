63b1a0000c91887f97edbf0102cb2e77859b1cc0
lxodud <xodud1998223@naver.com>
2024-02-01 00:44:05 +0900

    chore: Network 접근 수준 변경

--- aljo/Projects/Core/AJNetwork/Implementation/NetworkProvider.swift
+++ aljo/Projects/Core/AJNetwork/Implementation/NetworkProvider.swift
@@ -12,23 +12,23 @@ import Foundation
 import RxSwift
 import Alamofire
 
-struct NetworkProvider: Provider {
-  static func string(_ router: Router) -> Observable<String> {
+public struct NetworkProvider: Provider {
+  public static func string(_ router: Router) -> Observable<String> {
     return request(to: router).compactMap { String(data: $0, encoding: .utf8) }
   }
   
-  static func decodable<T: Decodable>(_ router: Router) -> Observable<T> {
+  public static func decodable<T: Decodable>(_ router: Router) -> Observable<T> {
     return request(to: router).decode(type: T.self, decoder: JSONDecoder())
   }
   
-  static func data(_ router: Router) -> Observable<Data> {
+  public static func data(_ router: Router) -> Observable<Data> {
     return request(to: router)
   }
 }
 
 // MARK: - Private
 extension NetworkProvider {
-  static func request(to router: Router) -> Observable<Data> {
+  private static func request(to router: Router) -> Observable<Data> {
     if case let .uploadMultipartFormData(multipartFormData, _) = router.behavior {
       return upload(to: router, with: multipartFormData)
     }
@@ -51,7 +51,7 @@ extension NetworkProvider {
     }
   }
   
-  static func upload(to router: Router, with multipartFormData: MultipartFormData) -> Observable<Data> {
+  private static func upload(to router: Router, with multipartFormData: MultipartFormData) -> Observable<Data> {
     return Observable.create { emitter in
       let request = AF.upload(multipartFormData: multipartFormData, with: router)
         .validate()
