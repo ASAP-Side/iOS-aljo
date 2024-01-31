0031f75e531c0e535f316aa3344bfa4ba3bdc425
lxodud <xodud1998223@naver.com>
2024-01-31 23:18:52 +0900

    feat: AJNetwork Router 구현

---
+++ aljo/Projects/Core/AJNetwork/Interface/Router.swift
@@ -0,0 +1,41 @@
+//
+//  Router.swift
+//  AJNetworkInterface
+//
+//  Created by 이태영 on 1/31/24.
+//  Copyright © 2024 com.ASAP. All rights reserved.
+//
+import Foundation
+
+import Alamofire
+
+protocol Router: URLRequestConvertible {
+  var baseURL: String { get }
+  var method: HTTPMethod { get }
+  var path: String { get }
+  var headers: HTTPHeaders { get }
+  var behavior: RequestBehavior { get }
+}
+
+extension Router {
+  func asURLRequest() throws -> URLRequest {
+    let urlString = baseURL + path
+    var request = try URLRequest(url: urlString, method: method, headers: headers)
+    
+    switch behavior {
+    case let .requestURLEncoded(parameters):
+      request = try URLEncoding.httpBody.encode(request, with: parameters)
+      
+    case let .requestJsonEncodable(parameters):
+      request = try JSONParameterEncoder.default.encode(parameters, into: request)
+      
+    case let .requestParameters(parameters, encoding):
+      request = try encoding.encode(request, with: parameters)
+      
+    default:
+      break
+    }
+    
+    return request
+  }
+}
