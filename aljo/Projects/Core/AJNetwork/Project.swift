96ced80d8086aa4c0c889aba8250bb8c48045248
lxodud <xodud1998223@naver.com>
2024-02-01 00:00:01 +0900

    config: AJNetwork Interface, Testing 외부 의존성 추가

--- aljo/Projects/Core/AJNetwork/Project.swift
+++ aljo/Projects/Core/AJNetwork/Project.swift
@@ -8,7 +8,13 @@ import EnvironmentPlugin
 let project = Project.app(
   to: "AJNetwork",
   targets: [
-    .interface(module: .core(.AJNetwork)),
+    .interface(
+      module: .core(.AJNetwork),
+      dependencies: [
+        .external(name: "Alamofire"),
+        .external(name: "RxSwift")
+      ]
+    ),
     .implements(
       module: .core(.AJNetwork),
       dependencies: [
@@ -21,7 +27,9 @@ let project = Project.app(
       module: .core(.AJNetwork),
       product: .staticLibrary,
       dependencies: [
-        .core(target: .AJNetwork, type: .interface)
+        .core(target: .AJNetwork, type: .interface),
+        .external(name: "Alamofire"),
+        .external(name: "RxSwift")
       ]
     ),
     .tests(
