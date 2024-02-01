53ab89abe0482517b891615e24dfb17f23eb9011
lxodud <xodud1998223@naver.com>
2024-02-02 02:23:11 +0900

    config: AJCoreData Target 추가

--- aljo/Projects/Core/AJCoreData/Project.swift
+++ aljo/Projects/Core/AJCoreData/Project.swift
@@ -7,5 +7,10 @@ import EnvironmentPlugin
 
 let project = Project.app(
   to: "AJCoreData",
-  targets: []
+  targets: [
+    .interface(module: .core(.AJCoreData)),
+    .implements(module: .core(.AJCoreData)),
+    .testing(module: .core(.AJCoreData), product: .framework),
+    .tests(module: .core(.AJCoreData), product: .unitTests)
+  ]
 )
