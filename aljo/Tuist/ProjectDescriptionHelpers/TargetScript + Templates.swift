1baf8dc2c255b3df1749acdd8f86d36db2f03b81
leegyoungmin <cow970814@naver.com>
2024-02-01 17:07:45 +0900

    config : SwiftLint 스크립트 재정의 및 적용

--- aljo/Tuist/ProjectDescriptionHelpers/TargetScript + Templates.swift
+++ aljo/Tuist/ProjectDescriptionHelpers/TargetScript + Templates.swift
@@ -1,22 +1,8 @@
 import ProjectDescription
 
 public extension TargetScript {
-  static let lintScript: String = """
-if test -d "/opt/homebrew/bin/"; then
-    PATH="/opt/homebrew/bin/:${PATH}"
-fi
-
-export PATH
-
-if which swiftlint > /dev/null; then
-    swiftlint
-else
-    echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint"
-fi
-"""
-  
   static let swiftLintTargetScript: TargetScript = .pre(
-    script: lintScript,
+    path: Path.relativeToRoot("Scripts/SwiftLintRunScript.sh"),
     name: "Check Lint From SwiftLint",
     basedOnDependencyAnalysis: false
   )
