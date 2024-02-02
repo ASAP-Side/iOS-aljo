da717b3261ba38071865461ddd4f1f71bfccb50c
lxodud <xodud1998223@naver.com>
2024-02-03 00:30:42 +0900

    feat: DefaultCoreDataStorage 구현

---
+++ aljo/Projects/Core/AJCoreData/Interface/CoreDataError.swift
@@ -0,0 +1,11 @@
+//
+//  CoreDataError.swift
+//  AJCoreDataInterface
+//
+//  Created by 이태영 on 2/3/24.
+//  Copyright © 2024 com.ASAP. All rights reserved.
+//
+
+enum CoreDataError: Error {
+  case didNotConvertDomain
+}
