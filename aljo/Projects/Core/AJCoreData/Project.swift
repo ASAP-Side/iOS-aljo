a3f14fe2b7b49b9dd58231de81fd3d96a51ea7ad
lxodud <xodud1998223@naver.com>
2024-02-02 22:06:26 +0900

    config: AJCoreData Interface RxSwift 의존성 추가

--- aljo/Projects/Core/AJCoreData/Project.swift
+++ aljo/Projects/Core/AJCoreData/Project.swift
@@ -8,7 +8,12 @@ import EnvironmentPlugin
 let project = Project.app(
   to: "AJCoreData",
   targets: [
-    .interface(module: .core(.AJCoreData)),
+    .interface(
+      module: .core(.AJCoreData),
+      dependencies: [
+        .external(name: "RxSwift")
+      ]
+    ),
     .implements(module: .core(.AJCoreData)),
     .testing(module: .core(.AJCoreData), product: .framework),
     .tests(module: .core(.AJCoreData), product: .unitTests)
