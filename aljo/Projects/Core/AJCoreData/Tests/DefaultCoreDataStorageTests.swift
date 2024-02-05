e040ef56d4b9fc04a67c570f8040aa30d1bea18c
lxodud <xodud1998223@naver.com>
2024-02-05 22:22:05 +0900

    test: 테스트 메서드에서 RxBlocking으로 리팩토링

--- aljo/Projects/Core/AJCoreData/Tests/DefaultCoreDataStorageTests.swift
+++ aljo/Projects/Core/AJCoreData/Tests/DefaultCoreDataStorageTests.swift
@@ -2,7 +2,9 @@ import XCTest
 @testable import AJCoreDataImplemenetation
 @testable import AJCoreDataTesting
 
+import RxBlocking
 import RxSwift
+
 
 final class DefaultCoreDataStorageTests: XCTestCase {
   private let sut = DefaultCoreDataStorage(worker: StubCoreDataWorker.default)
@@ -16,35 +18,32 @@ final class DefaultCoreDataStorageTests: XCTestCase {
   }
   
   // MARK: Test Method
-  func test_새로운데이터를넣고다시가져왔을때동일한id의데이터가나와야한다() {
+  func test_새로운데이터를넣고다시가져왔을때동일한id의데이터가나와야한다() throws {
     // given
     let dummy = DummyTestEntity(id: "1", title: "hi")
     let expectation = XCTestExpectation(description: "DefaultCoreDataStorageTest")
     
     // when
     var result: DummyTestEntity?
-    sut.upsert(entities: [dummy])
+    let observable = sut.upsert(entities: [dummy])
       .flatMap { _ -> Observable<[DummyTestEntity]> in
         return self.sut.fetch(with: nil, sortDescriptors: nil, limit: 1)
       }
       .compactMap {
         $0.first
       }
-      .subscribe(onNext: {
-        result = $0
-        expectation.fulfill()
-      }, onError: {
-        XCTFail($0.localizedDescription)
-      })
-      .disposed(by: disposeBag)
     
     // then
-    wait(for: [expectation], timeout: 5.0)
-    XCTAssertTrue(dummy.id == result?.id)
-    XCTAssertTrue(dummy.title == result?.title)
+    guard let result = try observable.toBlocking().first() else {
+      XCTFail("Fail")
+      return
+    }
+    
+    XCTAssertTrue(dummy.id == result.id)
+    XCTAssertTrue(dummy.title == result.title)
   }
   
-  func test_기존의데이터를덮어씌우고다시가져왔을때동일한데이터가나와야한다() {
+  func test_기존의데이터를덮어씌우고다시가져왔을때동일한데이터가나와야한다() throws {
     // given
     let dummies = [
       DummyTestEntity(id: "1", title: "hi"),
@@ -53,7 +52,7 @@ final class DefaultCoreDataStorageTests: XCTestCase {
     let expectation = XCTestExpectation(description: "DefaultCoreDataStorageTest")
     
     // when
-    sut.upsert(entities: dummies)
+    let observable = sut.upsert(entities: dummies)
       .flatMap {
         return self.sut.upsert(entities: [DummyTestEntity(id: "2", title: "hoho")])
       }
@@ -65,20 +64,18 @@ final class DefaultCoreDataStorageTests: XCTestCase {
       .compactMap {
         $0.first
       }
-      .subscribe(onNext: {
-        XCTAssertTrue(dummies.last?.id == $0.id)
-        XCTAssertTrue("hoho" == $0.title)
-        expectation.fulfill()
-      }, onError: {
-        XCTFail($0.localizedDescription)
-      })
-      .disposed(by: disposeBag)
-    
+
     // then
-    wait(for: [expectation], timeout: 5.0)
+    guard let result = try observable.toBlocking().first() else {
+      XCTFail("Fail")
+      return
+    }
+    
+    XCTAssertTrue(dummies.last?.id == result.id)
+    XCTAssertTrue("hoho" == result.title)
   }
   
-  func test_데이터를가져올때원하는정렬방식으로가져와야한다() {
+  func test_데이터를가져올때원하는정렬방식으로가져와야한다() throws {
     // given
     let dummies = [
       DummyTestEntity(id: "3", title: "ci"),
@@ -89,26 +86,21 @@ final class DefaultCoreDataStorageTests: XCTestCase {
     let expectation = XCTestExpectation(description: "DefaultCoreDataStorageTest")
     
     // when
-    var result: [DummyTestEntity] = []
     let sortDescriptor = NSSortDescriptor(key: "id", ascending: true)
-    sut.fetch(with: nil, sortDescriptors: [sortDescriptor], limit: nil)
-      .subscribe(onNext: {
-        result = $0
-        expectation.fulfill()
-      }, onError: {
-        XCTFail($0.localizedDescription)
-      })
-      .disposed(by: disposeBag)
-    
-    // then
-    wait(for: [expectation], timeout: 5.0)
+    let observable: Observable<[DummyTestEntity]> = sut.fetch(with: nil, sortDescriptors: [sortDescriptor], limit: nil)
 
+    // then
+    guard let result = try observable.toBlocking().first() else {
+      XCTFail("Fail")
+      return
+    }
+    
     for index in result.indices {
       XCTAssertTrue(index.description == result[index].id)
     }
   }
   
-  func test_동일한id의데이터만삭제되어야한다() {
+  func test_동일한id의데이터만삭제되어야한다() throws {
     // given
     let deleteData = DummyTestEntity(id: "2", title: "bi")
     let dummies = [
@@ -119,24 +111,23 @@ final class DefaultCoreDataStorageTests: XCTestCase {
     let expectation = XCTestExpectation(description: "DefaultCoreDataStorageTest")
     
     // when
-    sut.upsert(entities: dummies)
+    let observable = sut.upsert(entities: dummies)
       .flatMap {
         return self.sut.delete(entity: deleteData)
       }
       .flatMap { _ -> Observable<[DummyTestEntity]> in
         return self.sut.fetch(with: nil, sortDescriptors: nil, limit: nil)
       }
-      .subscribe(onNext: {
-        XCTAssertTrue($0.count == 1)
-        XCTAssertTrue($0.first?.id == "1")
-        XCTAssertTrue($0.first?.title == "hi")
-        expectation.fulfill()
-      }, onError: {
-        XCTFail($0.localizedDescription)
-      })
-      .disposed(by: disposeBag)
-      
-    wait(for: [expectation], timeout: 5.0)
+    
+    // then
+    guard let result = try observable.toBlocking().first() else {
+      XCTFail("Fail")
+      return
+    }
+    
+    XCTAssertTrue(result.count == 1)
+    XCTAssertTrue(result.first?.id == "1")
+    XCTAssertTrue(result.first?.title == "hi")
   }
   
   // MARK: Private Test Helper
