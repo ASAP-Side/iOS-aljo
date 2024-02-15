66320155959227e64e77d29db971bf0f8d5e7f31
leegyoungmin <cow970814@naver.com>
2024-02-15 22:15:42 +0900

    feat : Icon 관련 이미지 확장 메서드 구현

--- aljo/Projects/UserInterface/ASAPKit/Sources/Configurations/Color + Extension.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Color + Extension.swift
-//  ASAPKit
-//
-//  Copyright (c) 2024 Minii All rights reserved.
-        
-
-import Foundation
---
+++ aljo/Projects/UserInterface/ASAPKit/Sources/Configurations/Icon + Extension.swift
@@ -0,0 +1,31 @@
+//
+//  Color + Extension.swift
+//  ASAPKit
+//
+//  Copyright (c) 2024 Minii All rights reserved.
+
+import UIKit
+
+extension UIImage {
+  enum Icon {
+    static let arrow_circle = ASAPKitAsset.Arrow.circle.image
+    static let arrow_down = ASAPKitAsset.Arrow.down.image
+    static let arrow_left = ASAPKitAsset.Arrow.leftFill.image
+    static let arrow_right = ASAPKitAsset.Arrow.right
+    
+    static let camera_black = ASAPKitAsset.Camera.black.image
+    static let camera_dark = ASAPKitAsset.Camera.dark.image
+    static let camera_gray = ASAPKitAsset.Camera.grey
+    
+    static let picture_color = ASAPKitAsset.Picture.color.image
+    static let picture_gray = ASAPKitAsset.Picture.grey.image
+    
+    static let minus = ASAPKitAsset.Symbols.minus.image
+    static let plus = ASAPKitAsset.Symbols.plus.image
+    static let push = ASAPKitAsset.Symbols.push.image
+    static let scope = ASAPKitAsset.Symbols.scope.image
+    
+    static let xmark_black = ASAPKitAsset.Xmark.black.image
+    static let xmark_circle = ASAPKitAsset.Xmark.circle.image
+  }
+}
