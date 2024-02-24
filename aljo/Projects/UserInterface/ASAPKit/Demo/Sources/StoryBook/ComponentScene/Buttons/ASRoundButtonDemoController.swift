0f9ad674eb7a0e9244696cbfc1529d40289ca1ca
leegyoungmin <cow970814@naver.com>
2024-02-24 23:38:19 +0900

    feat : Round Button 속성 추가 및 구현

--- aljo/Projects/UserInterface/ASAPKit/Demo/Sources/StoryBook/ComponentScene/Buttons/ASRoundButtonDemoController.swift
+++ aljo/Projects/UserInterface/ASAPKit/Demo/Sources/StoryBook/ComponentScene/Buttons/ASRoundButtonDemoController.swift
@@ -9,25 +9,41 @@ import UIKit
 import ASAPKit
 
 class ASRoundButtonDemoController: ComponentViewController {
-  private let roundButton: ASRoundButton = {
-    let button = ASRoundButton()
-    button.setTitle("월", for: .normal)
-    button.titleLabel?.font = .pretendard(.body3)
-    button.setTitleColor(.black, for: .normal)
-    button.layer.backgroundColor = UIColor.greenColor.cgColor
-    return button
+  private let stackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.axis = .horizontal
+    stackView.distribution = .fillEqually
+    stackView.alignment = .fill
+    stackView.spacing = 10
+    stackView.isLayoutMarginsRelativeArrangement = true
+    stackView.layoutMargins = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
+    return stackView
   }()
   
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
     
-    view.addSubview(roundButton)
+    view.addSubview(stackView)
     
-    roundButton.snp.makeConstraints {
-      $0.center.equalToSuperview()
-      $0.height.equalTo(30)
-      $0.width.equalTo(roundButton.snp.height)
+    stackView.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview()
     }
+    
+    for idx in 0..<7 {
+      let button = generateButton(idx)
+      stackView.addArrangedSubview(button)
+    }
+  }
+  
+  func generateButton(_ idx: Int) -> ASRoundButton {
+    let button = ASRoundButton()
+    let title = Calendar.current.veryShortWeekdaySymbols[idx]
+    button.title = title
+    button.titleColor = .gray02
+    button.font = .pretendard(.body3)
+    button.backgroundColor = .red
+    button.borderColor = .gray02
+    return button
   }
 }
--- aljo/Projects/UserInterface/ASAPKit/Sources/Components/ASButton/ASRoundButton.swift
+++ aljo/Projects/UserInterface/ASAPKit/Sources/Components/ASButton/ASRoundButton.swift
@@ -7,8 +7,45 @@
 import UIKit
 
 public class ASRoundButton: UIButton {
+  private var paddingValue: NSDirectionalEdgeInsets {
+    return NSDirectionalEdgeInsets(top: 7, leading: 7, bottom: 7, trailing: 7)
+  }
+  
+  public var title: String? {
+    get { configuration?.title }
+    set { configuration?.title = newValue }
+  }
+  
+  public var titleColor: UIColor? {
+    get { configuration?.baseForegroundColor }
+    set { configuration?.baseForegroundColor = newValue }
+  }
+  
+  public override var backgroundColor: UIColor? {
+    get { configuration?.background.backgroundColor }
+    set { configuration?.background.backgroundColor = newValue }
+  }
+  
+  public var borderColor: UIColor? {
+    get { configuration?.background.strokeColor }
+    set { configuration?.background.strokeColor = newValue }
+  }
+  
+  public var font: UIFont? {
+    get { configuration?.attributedTitle?.font }
+    set { configuration?.attributedTitle?.font = newValue }
+  }
+  
   public init() {
     super.init(frame: .zero)
+    
+    var configuration = UIButton.Configuration.plain()
+    configuration.titleAlignment = .center
+    configuration.background.strokeWidth = 1
+    configuration.contentInsets = paddingValue
+    configuration.cornerStyle = .fixed
+    self.configuration = configuration
+    translatesAutoresizingMaskIntoConstraints = false
   }
   
   @available(*, unavailable, message: "사용할 수 없습니다.")
@@ -16,9 +53,16 @@ public class ASRoundButton: UIButton {
     fatalError("init(coder:) has not been implemented")
   }
   
-  public override func draw(_ rect: CGRect) {
-    super.draw(rect)
+  public override func layoutSubviews() {
+    super.layoutSubviews()
     
-    layer.cornerRadius = max(rect.width, rect.height) / 2
+    let maxValue = max(bounds.width, bounds.height)
+    bounds.size = CGSize(width: maxValue, height: maxValue)
+  }
+  
+  public override func draw(_ rect: CGRect) {
+    super.draw(rect.standardized)
+    
+    configuration?.background.cornerRadius = max(rect.width, rect.height) / 2
   }
 }
