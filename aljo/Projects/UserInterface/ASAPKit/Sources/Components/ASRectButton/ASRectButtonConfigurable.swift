a54b3c5e5a10f38f73e51852bd8d84208b5569a1
lxodud <xodud1998223@naver.com>
2024-02-28 01:40:16 +0900

    refactor: ASRectButtonConfigurable 주로 사용되는 요소들로 변경

--- aljo/Projects/UserInterface/ASAPKit/Sources/Components/ASRectButton/ASRectButtonConfigurable.swift
+++ aljo/Projects/UserInterface/ASAPKit/Sources/Components/ASRectButton/ASRectButtonConfigurable.swift
@@ -9,24 +9,75 @@
 import UIKit
 
 protocol ASRectButtonConfigurable {
+  var contentInsets: NSDirectionalEdgeInsets? { get set }
+  
   var title: String? { get set }
-  var titleAlignment: UIButton.Configuration.TitleAlignment { get set }
   var font: UIFont? { get set }
+  var titleAlignment: UIButton.Configuration.TitleAlignment? { get set }
   
   var image: UIImage? { get set }
-  var imagePlacement: NSDirectionalRectEdge { get set }
-  var imagePadding: ImagePaddingStyle { get set }
+  var imagePlacement: NSDirectionalRectEdge? { get set }
+  var imagePadding: ImagePaddingStyle? { get set }
   
+  var backgroundColor: UIColor? { get set }
   var cornerRadius: CGFloat? { get set }
-  var strokeWidth: CGFloat { get set }
+  var strokeWidth: CGFloat? { get set }
+  var strokeColor: UIColor? { get set }
   
-  var isNormalHandler: ((UIButton.Configuration) -> Void)? { get set }
-  var isSelectedHandler: ((UIButton.Configuration) -> Void)? { get set }
-  var isDisabledHandler: ((UIButton.Configuration) -> Void)? { get set }
-  var isHighlightedHandler: ((UIButton.Configuration) -> Void)? { get set }
+  var normalHandler: ((UIButton) -> Void)? { get set }
+  var selectedHandler: ((UIButton) -> Void)? { get set }
+  var disabledHandler: ((UIButton) -> Void)? { get set }
+  var highlightedHandler: ((UIButton) -> Void)? { get set }
+  
+  func makeConfiguration() -> UIButton.Configuration
+}
+
+extension ASRectButtonConfigurable {
+  func makeConfiguration() -> UIButton.Configuration {
+    var configuration = UIButton.Configuration.plain()
+    if let contentInsets = contentInsets {
+      configuration.contentInsets = contentInsets
+    }
+    
+    if let titleAlignment = titleAlignment {
+      configuration.titleAlignment = titleAlignment
+    }
+    if let title = title {
+      var titleContainer = AttributeContainer()
+      titleContainer.font = font
+      configuration.attributedTitle = AttributedString(title, attributes: titleContainer)
+    }
+    
+    if let image = image {
+      configuration.image = image
+    }
+    if let imagePlacement = imagePlacement {
+      configuration.imagePlacement = imagePlacement
+    }
+    if case let .fixed(padding) = imagePadding {
+      configuration.imagePadding = padding
+    }
+    
+    var backgroundCofiguration = UIBackgroundConfiguration.clear()
+    if let backgroundColor = backgroundColor {
+      backgroundCofiguration.backgroundColor = backgroundColor
+    }
+    if let cornerRadius = cornerRadius {
+      backgroundCofiguration.cornerRadius = cornerRadius
+    }
+    if let strokeWidth = strokeWidth {
+      backgroundCofiguration.strokeWidth = strokeWidth
+    }
+    if let strokeColor = strokeColor {
+      backgroundCofiguration.strokeColor = strokeColor
+    }
+    configuration.background = backgroundCofiguration
+    
+    return configuration
+  }
 }
 
 enum ImagePaddingStyle {
-  case fixed(padding: Int)
+  case fixed(padding: CGFloat)
   case dynamic
 }
