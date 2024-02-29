8e295acf2b42cc3dede8913ca7cb01bf2d3206c1
leegyoungmin <cow970814@naver.com>
2024-02-29 16:09:20 +0900

    feat : Slider 버튼 필요 아이콘 추가

--- aljo/Projects/UserInterface/ASAPKit/Sources/Components/ASSlider/ASSlider.swift
+++ aljo/Projects/UserInterface/ASAPKit/Sources/Components/ASSlider/ASSlider.swift
@@ -7,5 +7,34 @@
 import UIKit
 
 public final class ASSlider: UISlider {
+  private let trackLayer = CALayer()
   
+  public override func draw(_ rect: CGRect) {
+    super.draw(rect)
+    
+    clear()
+    createTrack()
+    
+    createTrackLabel()
+  }
+  
+  private func clear() {
+    tintColor = .clear
+    maximumTrackTintColor = .clear
+    backgroundColor = .clear
+    thumbTintColor = .clear
+  }
+  
+  private func createTrackLabel() {
+    maximumValueImage = .Icon.sound
+    minimumValueImage = .Icon.mute
+  }
+  
+  private func createTrack() {
+    trackLayer.masksToBounds = true
+    trackLayer.backgroundColor = UIColor.gray03.cgColor
+    trackLayer.frame = .init(x: .zero, y: frame.height / 4, width: frame.width, height: 4)
+    trackLayer.cornerRadius = min(trackLayer.frame.width, trackLayer.frame.height) / 2
+    layer.insertSublayer(trackLayer, at: 0)
+  }
 }
--- aljo/Projects/UserInterface/ASAPKit/Sources/Configurations/Icon + Extension.swift
+++ aljo/Projects/UserInterface/ASAPKit/Sources/Configurations/Icon + Extension.swift
@@ -9,25 +9,32 @@ import UIKit
 // swiftlint:disable all
 public extension UIImage {
   enum Icon {
+    // arrow
     public static let arrow_circle: UIImage = ASAPKitAsset.Arrow.circle.image
     public static let arrow_down: UIImage = ASAPKitAsset.Arrow.down.image
     public static let arrow_left: UIImage = ASAPKitAsset.Arrow.leftFill.image
     public static let arrow_right: UIImage = ASAPKitAsset.Arrow.right.image
     
+    // camera
     public static let camera_black: UIImage = ASAPKitAsset.Camera.cameraBlack.image
     public static let camera_dark: UIImage = ASAPKitAsset.Camera.cameraDark.image
     public static let camera_gray: UIImage = ASAPKitAsset.Camera.cameraGray.image
     
+    // picture
     public static let picture_color: UIImage = ASAPKitAsset.Picture.pictureColor.image
     public static let picture_gray: UIImage = ASAPKitAsset.Picture.pictureGray.image
     
+    // symbols
     public static let check: UIImage = ASAPKitAsset.Symbols.check.image
     public static let check_small: UIImage = ASAPKitAsset.Symbols.checkSmall.image
     public static let minus: UIImage = ASAPKitAsset.Symbols.minus.image
     public static let plus: UIImage = ASAPKitAsset.Symbols.plus.image
     public static let push: UIImage = ASAPKitAsset.Symbols.push.image
     public static let scope: UIImage = ASAPKitAsset.Symbols.scope.image
+    public static let mute: UIImage = ASAPKitAsset.Symbols.soundMute.image
+    public static let sound: UIImage = ASAPKitAsset.Symbols.sound.image
     
+    // xmark
     public static let xmark_black: UIImage = ASAPKitAsset.Xmark.removeBlack.image
     public static let xmark_circle: UIImage = ASAPKitAsset.Xmark.removeCircle.image
   }
