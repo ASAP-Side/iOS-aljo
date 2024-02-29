cdbc8adb61b94c6748645beb8dec200a03b44788
leegyoungmin <cow970814@naver.com>
2024-02-29 18:05:26 +0900

    feat : Slider 설정 기본 값으로 변경

--- aljo/Projects/UserInterface/ASAPKit/Sources/Components/ASSlider/ASSlider.swift
+++ aljo/Projects/UserInterface/ASAPKit/Sources/Components/ASSlider/ASSlider.swift
@@ -7,60 +7,25 @@
 import UIKit
 
 public final class ASSlider: UISlider {
-  private let baseLayer = CALayer()
-  private let trackLayer = CALayer()
-  
   public override func draw(_ rect: CGRect) {
     super.draw(rect)
     
-    clear()
-    
-    createTrackLabel()
-    createBaseLayer()
-    createTrackLayer()
+    configureItems()
   }
   
-  private func clear() {
-    tintColor = .clear
-    maximumTrackTintColor = .clear
+  private func configureItems() {
+    tintColor = .red01
+    maximumTrackTintColor = .gray03
     backgroundColor = .clear
-    thumbTintColor = .gray01
-  }
-  
-  private func createTrackLabel() {
+    thumbTintColor = .white
+    
     maximumValueImage = .Icon.sound
     minimumValueImage = .Icon.mute
   }
   
-  private func createBaseLayer() {
-    baseLayer.masksToBounds = true
-    baseLayer.backgroundColor = UIColor.gray03.cgColor
-    if let minSize = minimumValueImage?.size, let maxSize = maximumValueImage?.size {
-      baseLayer.frame = .init(
-        x: minSize.width + 16,
-        y: minSize.height / 2,
-        width: frame.width - minSize.width - maxSize.width - 32,
-        height: 4
-      )
-    }
-    baseLayer.cornerRadius = min(baseLayer.frame.width, baseLayer.frame.height) / 2
-    layer.insertSublayer(baseLayer, at: 0)
-  }
-  
-  func createTrackLayer() {
-    let color = UIColor.red01
-    trackLayer.backgroundColor = color.cgColor
-    let baseFrame = baseLayer.frame
-    
-    if let minSize = minimumValueImage?.size {
-      trackLayer.frame = .init(
-        x: minSize.width + 16,
-        y: minSize.height / 2,
-        width: .zero,
-        height: 4
-      )
-    }
-    trackLayer.cornerRadius = min(trackLayer.frame.width, trackLayer.frame.height) / 2
-    layer.insertSublayer(trackLayer, at: 1)
+  public override func trackRect(forBounds bounds: CGRect) -> CGRect {
+    var rect = super.trackRect(forBounds: bounds)
+    rect.size.height = 4
+    return rect
   }
 }
