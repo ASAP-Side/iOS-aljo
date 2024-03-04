6fb335b250b49111c63a01270ec0420f9a1abfcc
leegyoungmin <cow970814@naver.com>
2024-03-04 22:47:52 +0900

    feat : Slider Demo View Controller 구현

--- aljo/Projects/UserInterface/ASAPKit/Demo/Sources/StoryBook/ComponentScene/Sliders/ASSliderDemoController.swift
+++ aljo/Projects/UserInterface/ASAPKit/Demo/Sources/StoryBook/ComponentScene/Sliders/ASSliderDemoController.swift
@@ -6,9 +6,19 @@
 
 import UIKit
 
+import RxSwift
+
 import ASAPKit
 
 class ASSliderDemoController: UIViewController {
+  private let label: UILabel = {
+    let label = UILabel()
+    label.textColor = .blue
+    label.font = .pretendard(.body6)
+    label.textAlignment = .center
+    return label
+  }()
+  
   private let slider: ASSlider = {
     let slider = ASSlider(height: .small)
     slider.backgroundTintColor = .clear
@@ -19,12 +29,46 @@ class ASSliderDemoController: UIViewController {
     return slider
   }()
   
+  private var disposeBag = DisposeBag()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    view.backgroundColor = .systemBackground
+    configureUI()
     
-    view.addSubview(slider)
+    binding()
+  }
+}
+
+private extension ASSliderDemoController {
+  func binding() {
+    slider.rx.value.changed
+      .asObservable()
+      .compactMap { round($0 * 10) / 10.0 }
+      .map { String($0) }
+      .bind(to: label.rx.text)
+      .disposed(by: disposeBag)
+  }
+}
+
+private extension ASSliderDemoController {
+  func configureUI() {
+    view.backgroundColor = .systemBackground
+
+    configureHierarchy()
+    makeConstraints()
+  }
+  
+  func configureHierarchy() {
+    [label, slider].forEach(view.addSubview)
+  }
+  
+  func makeConstraints() {
+    label.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+      $0.centerX.equalToSuperview()
+      $0.leading.trailing.equalToSuperview().inset(16)
+    }
     
     slider.snp.makeConstraints {
       $0.center.equalToSuperview()
