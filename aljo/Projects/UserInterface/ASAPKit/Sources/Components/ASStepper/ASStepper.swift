dd6eb7f9b76208d9435dae6b0f7e0c478e2923c3
Hamo <85005933+lxodud@users.noreply.github.com>
2024-03-05 19:51:01 +0900

    chore: available 추가
    Co-authored-by: 미니 <52390923+leegyoungmin@users.noreply.github.com>

--- aljo/Projects/UserInterface/ASAPKit/Sources/Components/ASStepper/ASStepper.swift
+++ aljo/Projects/UserInterface/ASAPKit/Sources/Components/ASStepper/ASStepper.swift
@@ -60,6 +60,7 @@ public final class ASStepper: UIView {
     bind()
   }
   
+  @available(*, unavailable, message: "스토리 보드로 생성할 수 없습니다.")
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
