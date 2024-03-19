744abfcb0cecbdb11882fc571c512e8461f3adcd
lxodud <xodud1998223@naver.com>
2024-03-19 17:27:57 +0900

    feat: AlertTransitionDelegate 구현

---
+++ aljo/Projects/UserInterface/ASAPKit/Sources/Controllers/ASAlert/AlertTransitionDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AlertTransitionDelegate.swift
+//  ASAPKit
+//
+//  Created by 이태영 on 3/19/24.
+//  Copyright © 2024 com.ASAP. All rights reserved.
+//
+
+import UIKit
+
+public class AlertTransitionDelegate: NSObject, UIViewControllerTransitioningDelegate {
+  public func animationController(
+    forPresented presented: UIViewController,
+    presenting: UIViewController,
+    source: UIViewController
+  ) -> UIViewControllerAnimatedTransitioning? {
+    return AlertTransitionAnimator(
+      transitionStyle: .presentation
+    )
+  }
+  
+  public func animationController(
+    forDismissed dismissed: UIViewController
+  ) -> UIViewControllerAnimatedTransitioning? {
+    return AlertTransitionAnimator(
+      transitionStyle: .dismissal
+    )
+  }
+}
